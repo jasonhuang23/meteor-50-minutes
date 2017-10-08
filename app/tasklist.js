0e9e164e39c077bcd4e9cb5ed8d172e02f6ca275
Hello2343 <jasonhuang2343@gmail.com>
2017-10-07 23:06:45 -1000

    Task 2: Tasklist setup

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,31 +1,13 @@
 Tasks = new Mongo.Collection('tasks');
 
-if (Meteor.isClient) {
-  Template.tasks.helpers({
-    tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
-
-  Template.tasks.events({
-    "submit .add-task": function(event) {
-      let name = event.target.name.value;
-      Tasks.insert({
-        name: name, createdAt: new Date()
-      });
-      event.target.name.value = '';
-      return false;
-    },
-
-    "click .delete-task": function(event) {
-      if (confirm('Delete Task?')) {
-        Tasks.remove(this._id);
-      }
-      return false;
-    }
-  })
+if (Meteor.isClient){
+Template.tasks.helpers({
+  tasks: function(){
+    return Tasks.find({}, {sort: {createdAt: -1}});
+  }
+})
 }
 
-if (Meteor.isServer) {
+if (Meteor.isServer){
 
 }
